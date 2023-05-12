d38317de437a2ffe060b12cbc30fe10d6ac2d49f
reyn-nova <rynvva@gmail.com>
2023-05-12 13:13:21 +0700

    fix: change openpath framework to openpath M1

---
+++ VendoredIOSFrameworks/Allegion_Access_Hub_iOS.xcframework/ios-arm64/Allegion_Access_Hub_iOS.framework/Headers/Allegion.Access.Hub.iOS-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Allegion_Access_Hub_iOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char Allegion_Access_Hub_iOSVersionString[];
+
